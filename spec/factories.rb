0b21fb6d1cde107edb856072a450423896a6d56b
vijendra <vijendrakarkala@gmail.com>
2012-02-04 22:37:51 +0530

    unit test for data row model. For issue #13

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,19 +3,44 @@ FactoryGirl.define do
     uploaded_data { file_upload('test1.csv') }
     col_sep ';'
     quote_char '"'
-    association :mapping
+    association :mapping, :factory => :mapping
   end
   
   factory :mapping do
+ 
   end
   
   factory :mapping_element do
-    association :mapping
+    mapping
     source 0
     target 'organization'
+  end
+  
+  factory :email_mapping_element, :class => MappingElement  do
+    mapping
+    source 2
+    target 'email'
+  end
+  
+  factory :gender_mapping_element, :class => MappingElement do
+    association :mapping, :factory => :mapping
+    source 3
+    target 'gender'
+    conv_type 'enum'
+    conv_opts  "{\"male\":\"Herr\",\" female\":\"Frau\"}"
+  end
+  
+  factory :birthday_mapping_element, :class => MappingElement do
+    association :mapping
+    source 4
+    target 'birthday'
+    conv_type 'date'
+    conv_opts "{\"date\":\"%Y-%M-%d\"}"
+ 
   end
   
   factory :import do
     association :attachment
   end
+ 
 end
--- spec/models/data_row_spec.rb
+++ spec/models/data_row_spec.rb
@@ -2,4 +2,47 @@ require 'spec_helper'
 
 describe DataRow do
   it { should belong_to(:import) }
+ 
+  describe "#populate_client" do
+    before :each do
+      @mapping = Factory(:mapping)
+      @attachment = Factory(:attachment, :uploaded_data => file_upload('valid_data.csv'), mapping: @mapping)
+ 
+      @import = Factory.build(:import, attachment: @attachment)
+      @import.should_receive(:populate_data_rows).and_return(true)  #To avoid firing data_row create
+      @import.save
+      @client = stub_sk_client
+    end
+    
+    context 'creating sk client record' do
+      before :each do
+        Factory(:mapping_element,  mapping: @mapping)
+        Factory(:gender_mapping_element,  mapping: @mapping)
+        Factory(:birthday_mapping_element,  mapping: @mapping)
+      end
+      
+      context 'successful' do
+        before :each do
+          @client.should_receive(:save).and_return(true)
+          @client.should_receive(:id).and_return("some_uuid")
+          @data_row = @import.data_rows.new(data: @attachment.rows[1])
+          @data_row.save!
+        end
+        
+        it "should save Client ID in corresponding data row" do
+          @data_row.sk_id.should == "some_uuid"
+        end
+        
+        it "should have converted enum field value" do
+          @client.gender.should == "male"
+        end
+        
+        it "should have formatted date field value" do
+          @client.birthday.should == "1980.01.10"
+        end
+        
+      end  
+    end
+ 
+  end
 end
