2ad7a167926ebd4e19e7124c5a5e80127b36040f
vijendra <vijendrakarkala@gmail.com>
2012-02-05 10:58:24 +0530

    improved unit tests for data_row model. Related to issue #13

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,41 +3,50 @@ FactoryGirl.define do
     uploaded_data { file_upload('test1.csv') }
     col_sep ';'
     quote_char '"'
-    association :mapping, :factory => :mapping
+    association :mapping
   end
   
   factory :mapping do
  
   end
   
+  #data rwo will be like this ["Test organization", "Test", "male_user", "test_user1@email.com", "Herr", "1980-01-10"]
   factory :mapping_element do
     mapping
     source 0
     target 'organization'
   end
   
+  factory :fname_mapping_element, :class => MappingElement do
+    mapping
+    source '1,2'
+    target 'first_name'
+    conv_type 'join'
+  end
+  
   factory :email_mapping_element, :class => MappingElement  do
     mapping
-    source 2
+    source 3
     target 'email'
   end
   
   factory :gender_mapping_element, :class => MappingElement do
-    association :mapping, :factory => :mapping
-    source 3
+    mapping
+    source 4
     target 'gender'
     conv_type 'enum'
     conv_opts  "{\"male\":\"Herr\",\" female\":\"Frau\"}"
   end
   
   factory :birthday_mapping_element, :class => MappingElement do
-    association :mapping
-    source 4
+    mapping
+    source 5
     target 'birthday'
     conv_type 'date'
     conv_opts "{\"date\":\"%Y-%M-%d\"}"
- 
   end
+  
+  
   
   factory :import do
     association :attachment
