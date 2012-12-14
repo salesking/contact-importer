e6acca13e27d6d36c9093a026fbc33d9e0545cae
Georg Leciejewski <gl@salesking.eu>
2012-12-14 17:49:57 +0100

    dont init app on asset precompile

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ end
 
 module CsvImporter
   class Application < Rails::Application
-    
+
     config.middleware.use 'Rack::RawUpload', :paths => ['/attachments']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -53,5 +53,6 @@ module CsvImporter
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
   end
 end
