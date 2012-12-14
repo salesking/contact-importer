01f947c6a6e62b84be24c2c77f9233c0f0e03233
Georg Leciejewski <gl@salesking.eu>
2012-12-14 18:07:14 +0100

    try fix postgres group by syntax

--- app/models/mapping.rb
+++ app/models/mapping.rb
@@ -4,16 +4,16 @@ class Mapping < ActiveRecord::Base
 
   has_many :mapping_elements, dependent: :destroy
   has_many :attachments, dependent: :nullify
-  
+
   default_scope order('mappings.id desc')
-  
+
   accepts_nested_attributes_for :mapping_elements
   scope :by_company, lambda{|company_id| where(:company_id => company_id)}
   scope :with_fields, joins(:mapping_elements).
                       select('mappings.id, count(mapping_elements.id) as element_count').
-                      group('mappings.id').
+                      group('mappings.id, element_count').
                       having('element_count > 0')
-                                                         
+
   def title
     I18n.t('mappings.title', count: mapping_elements.count, fields: mapping_elements.collect(&:target).to_sentence)
   end
