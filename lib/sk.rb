6b6f03452e034ed4fb18b459780acc7408bd6459
Mike Połtyn <mike@poltyn.com>
2014-02-01 14:25:29 +0000

    allow CSV importer to change imported type of a Contact

--- lib/sk.rb
+++ lib/sk.rb
@@ -2,35 +2,35 @@ require 'sk_api_schema'
 require 'sk_sdk/base'
 require 'sk_sdk/oauth'
 # Tiny helper class for talking to SalesKing
-# - Setup local Sk:: classes for remote objects. client, address,..
+# - Setup local Sk:: classes for remote objects. contact, address,..
 # - construct writable fields
 class Sk
   # setup oAuth app info, local classes
   @@conf = YAML.load_file(Rails.root.join('config', 'salesking_app.yml'))
   #raise 'config/salesking_app.yml missing' if !@@conf || @@conf.empty?
   APP = SK::SDK::Oauth.new(@@conf[Rails.env])
-  %w{Client Address}.each do |model|
+  %w{Contact Address}.each do |model|
     eval "class #{model} < SK::SDK::Base;end"
   end
-  
+
   # init SalesKing classes and set connection oAuth token
   def self.init(site, token)
     SK::SDK::Base.set_connection( {:site => site, :token => token} )
   end
 
-  # Construct fields for importing clients. Hides unnecessary fields and adds
+  # Construct fields for importing contacts. Hides unnecessary fields and adds
   # address fields for one address
   #
   # == Returns
   #<Array>:: [ { 'field_name'=>{properties} }, ]
-  def self.client_fields
+  def self.contact_fields
     # skip fields that dont make sence
     exclude_cli = ['lock_version', 'team_id', 'addresses']
     exclude_adr = ['order', 'lat', 'long', '_destroy']
-    client_schema = SK::Api::Schema.read('client', '1.0')
+    contact_schema = SK::Api::Schema.read('contact', '1.0')
     adr_schema = SK::Api::Schema.read('address', '1.0')
     props = []
-    client_schema['properties'].each do |name, prop|
+    contact_schema['properties'].each do |name, prop|
       props << { name => prop } unless prop['readonly'] || exclude_cli.include?(name)
     end
     # only one address for now inline
