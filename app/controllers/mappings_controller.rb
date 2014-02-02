8ab66da8c8d0701b78d458cde8f431e517a5f957
Georg Leciejewski <gl@salesking.eu>
2014-02-02 16:02:35 +0100

    fix params for mappings/create

--- app/controllers/mappings_controller.rb
+++ app/controllers/mappings_controller.rb
@@ -1,9 +1,9 @@
 class MappingsController < ApplicationController
   load_and_authorize_resource :attachment, only: [:new, :create]
   load_and_authorize_resource
+  skip_load_resource only: [:create]
 
   def create
-    #TODO still get errors for nested params
     @mapping = Mapping.new(mapping_params)
     @mapping.user = current_user
     @mapping.attachments << @attachment
@@ -16,6 +16,6 @@ class MappingsController < ApplicationController
 
   private
   def mapping_params
-    params.require(:mapping).permit(:mapping_elements_attributes)
+    params.require(:mapping).permit(mapping_elements_attributes: [:id, :source, :target, :source, :conv_type, :conv_opts, :import_id])
   end
 end
