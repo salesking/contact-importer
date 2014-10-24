e4227095bb73387339c6c87f52724b5004dd0932
Robin Kunath <robin.kunath@gmail.com>
2014-10-24 10:46:13 +0200

    don't allow locale app to push locales

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -2,4 +2,6 @@ require 'localeapp/rails'
 
 Localeapp.configure do |config|
   config.api_key = 'sLujBU5awtTSVT3JG2ucZjncAqsGOY6G0IPZYwGUMUFeTEOWB0'
+  config.sending_environments = [] # Never send missing keys
+  config.polling_environments = [] 
 end
