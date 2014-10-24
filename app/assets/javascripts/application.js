e212489a81092f8745f4e270cd1212cb67bfc7da
Robin Kunath <robin.kunath@gmail.com>
2014-10-24 14:36:31 +0200

    add jquery ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 /* load jquery, rails.js, jquery ui from bower components => jquery-ujs/src/rails.js*/
 //= require jquery
+//= require jquery-ui
 //= require jquery-ujs
 //= require fileuploader
 //= require schmuugle/dist/js/schmuugle.js
